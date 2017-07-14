077ac79d5a4a6e6e8033e03b9380913893e64182
Hack Reactor Student <student@hackreactor.com>
2017-07-14 15:30:24 -0700

    Add functionality for ajax get

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,20 +2,36 @@
 var messages = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
 // POST request
 $(document).on("ready", function() {
-  $ajax({
+  $.ajax({
     url: messages,
     method:'GET',
     dataType: 'json',
-    success: onSuccess
+    success: onSuccess,
+  });
+
+  $.ajax({
+    url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes',
+    method:'POST',
+    dataType: 'json',
+    success: postSuccess,
   });
 
   function onSuccess(json) {
-    var features = json;
-    $("#info").append(features);
-    $("#chats1").append("<p>" + features + "</p>");
-    console.log(features);
-  }
+    var jsonArray = json.results;
+    $("#info").append(json);
+    $("#chats1").append("<p>" + json + "</p>");
+    console.log(jsonArray[14]);
+    for (var i = 0; i < jsonArray.length; i++) {
+      for (key in jsonArray[i]) {
+        var msg = (jsonArray[i]['text']);
+        $("#chatBox").append("<p>" + msg + "</p>");
+      }
+    }
+   }
+
+   function postSuccess()
 });
+
 
 //
 
