da9a5b409be3e8f973376eab085a69f1b404ca93
jsonlitz <jasondotlee@gmail.com>
2017-07-14 14:37:35 -0700

    change config to proper

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,27 @@
 // YOUR CODE HERE:
+var messages = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
+// POST request
+$(document).on("ready", function() {
+  $ajax({
+    url: messages,
+    method:'GET',
+    dataType: 'json',
+    success: onSuccess
+  });
+
+  function onSuccess(json) {
+    var features = json;
+    $("#info").append(features);
+    $("#chats1").append("<p>" + features + "</p>");
+    console.log(features);
+  }
+});
+
+//
+
+
+
+// var message = {
+//   username: 'json lee',
+//   text: 'ssssssuuuuuhhh dude',
+// };
